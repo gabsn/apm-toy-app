ec2b93eb29ab6c6000ce68fc4a1263e8462d5c2a
gabsn <gabin.marignier@datadoghq.com>
2017-08-02 16:15:02 -0400

    Fully working go/redis counter

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	redis "gopkg.in/redis.v4"
+	"github.com/go-redis/redis"
 )
 
 func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_URL"),
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+	router := newRouter()
+	http.HandleFunc("/redis", router.redisHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
+type Router struct {
+	redis *redis.Client
+}
+
+func newRouter() *Router {
+	return &Router{
+		redis.NewClient(&redis.Options{
+			Addr: os.Getenv("REDIS_ADDRESS"),
+		}),
+	}
+}
+
+func (r *Router) redisHandler(w http.ResponseWriter, req *http.Request) {
+	n := r.redis.Incr("counter").Val()
+	fmt.Fprintf(w, "/redis received %v hits.", n)
 }
